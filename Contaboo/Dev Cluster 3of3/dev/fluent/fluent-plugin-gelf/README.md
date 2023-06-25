940df044ff980062cf8f600237925591e43b64ba
Berin Mujčinović <berin.mujcinovic@edu.fit.ba>
2023-06-25 05:20:21 +0200

    Deleted clone directory

--- Contaboo/Dev Cluster 3of3/dev/fluent/fluent-plugin-gelf/README.md
+++
@@ -1,2 +0,0 @@
-fluent-plugin-gelf
-==================
--- Contaboo/Dev Cluster 3of3/dev/graylog/loadmsg/graylog-msg-script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo '{"version": "1.1","host":"graylog.example.com","short_message":"A short message that helps you identify what is going on","full_message":"Backtrace here\n\nmore stuff","level":1,"_user_id":9001,"_some_info":"foo","_some_env_var":"bar"}'
